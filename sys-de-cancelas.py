eec83d9e4db69412826615dcaa4db8268e692027
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2022-12-10 09:31:18 -0300

    Update sys-de-cancelas.py

--- sys-de-cancelas.py
+++ sys-de-cancelas.py
@@ -1,17 +1,21 @@
 from time import sleep
 
-print('---------- Sys_Cancelas ----------')
+dtl1 = ('-=-' * 10)
+dtl2 = (' ' * 6)
+print(f'{dtl1}\n{dtl2}Sistema de Canelas\n{dtl1}')
 print()
+sleep(1)
 
-placa = str(input('Qual é a sua placa:')).upper()
+
+placa = str(input('Qual é a sua placa: ')).upper()
 tag = str(input('Qual é o seu tag: '))
 print('FAZENDO A LEITURA......')
-sleep(3)
+sleep(2)
 
 if placa == 'KNO0F84':
   print('Acesso liberado!')
 else:
-  if tag == '0784':
+  if tag == '0584':
     print('Acesso Liberado!')
   else:
     print('Acesso Negado!')
