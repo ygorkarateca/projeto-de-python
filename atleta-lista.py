5c6a91418dec5c269207d3647784a6c28cf4b203
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2023-09-15 23:14:55 -0300

    VAI DA MERDA

---
+++ atleta-lista.py
@@ -0,0 +1,27 @@
+# Manipulado lista
+
+atletas = [
+            'Luciano Werneck', 
+            'Matheus Campina',
+            'Kaick Marlon',
+            'Paula Kiai',
+            'Lucas Souto',
+            'Matheus Dias',
+            'Gabriel Franco',
+            'Leonardo Milhares',
+            'Enryk Kiai',
+            'Ygor Silva',
+            'Rafael MSK',
+            'Ralf MSK',
+            'Danilo Maia',
+            'Phablo Kassiano'
+            ]
+        
+
+if ' Ygor Silva' in atletas:
+    print('sim está na lista')
+else:
+    print('Não está na lista')
+    
+comparecer = len(atletas)
+print(f'Total de atletas = {comparecer}')
