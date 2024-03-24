fd3f4e363a54eaeaef2b860f5c9f87271a5f25e9
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2024-03-24 06:40:24 -0300

    Update sys-ajuda-financeiro.py

--- sys-ajuda-financeiro.py
+++ sys-ajuda-financeiro.py
@@ -7,23 +7,28 @@ print(f"{caracteres}\n{espaco}Sistema bancário\n{caracteres}")
 sleep(1)
 print(' ')
 
-# Código para opção de depositar
-depoitar = ...
-
-# Código para a opção de sacar
-sacar = ...
-
 # Informação do salário do cliente
 salario_cliente = float(input("Informe seu salário: "))
 
-# Código de opções para o cliente
-opcao = print("Digite a opção desejada:\n|[1]Depositar|\n|[2]Sacar|\n|[3]Saldo|\n|[4]Sair|")
+while salario_cliente < 0:
+    if
 
-if opcao == 1:
-    pass
-elif opcao == 2:
-    pass
-elif opcao == 3:
-    print(f"Saldo atual: {salario_cliente}")
-else:
-    print("Opcão invalida!")
+    # Código de opções para o cliente
+        opcao = print("Digite a opção desejada:\n|[1]Depositar|\n|[2]Sacar|\n|[3]Saldo|\n|[4]Sair|")
+        
+    # Código para opção de depositar
+        depoitar = salario_cliente + ...
+
+    # Código para a opção de sacar
+        sacar = salario_cliente - ...
+
+        if opcao == 1:
+            pass
+        elif opcao == 2:
+            pass
+        elif opcao == 3:
+            print(f"Saldo atual: {salario_cliente}")
+        else:
+            print("Opcão invalida!")
+    else:
+        print("Operação Encerrada!\n Volte sempre!")
