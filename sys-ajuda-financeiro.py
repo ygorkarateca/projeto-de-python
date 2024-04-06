d0f36f46de7266b8e40e8121848df08c73cf2083
Ygor Fernandes <115374517+ygorkarateca@users.noreply.github.com>
2024-04-06 08:42:28 -0300

    brb

--- sys-ajuda-financeiro.py
+++ sys-ajuda-financeiro.py
@@ -3,6 +3,15 @@ from prettytable import PrettyTable as pt
 import os
 
 
+# Condições para o código
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saque = 0
+LIMITE_SAQUE = 5
+
+
 # Informação do salário do cliente
 salario_cliente = float(input("Informe seu salário: "))
 
@@ -11,35 +20,42 @@ salario_cliente = float(input("Informe seu salário: "))
 os.system('cls')
 
 # gráfico básico
-minha_tabela = pt(["-", "Saldo", "Depositar", "Sacar", "Sair"])
-minha_tabela.add_row(["Opções", "1", "2", "3", "4"])
+minha_tabela = pt(["-", "Saldo", "Depositar", "Sacar", "Extrato", "Sair"])
+minha_tabela.add_row(["Opções", "1", "2", "3", "4", "5"])
 print(minha_tabela)
-opcao = input("Escolha opção desejada: ")
 sleep(1)
 print()
 
-# Limpa o terminal
-os.system('cls')
 
-while opcao != "5":
+while True:
+
+    opcao = input("Escolha opção desejada: ")
+    # Limpa o terminal
+    os.system('cls')
 
     # Opção de visualizar o saldo
     if opcao == "1":
-        print(f"Saldo atual: {salario_cliente}")
+        print(f"Saldo atual: {salario_cliente:.2f}")
 
     # Opção de Depositar
     elif opcao == "2":
-        depositar = float(input("Qual valor para o deposito? "))
-        print(f"Você depositou: R${depositar}\nSaldo atual: R${salario_cliente + depositar}")
+        depositar = float(input("Qual valor para deposito? "))
+        
+        if valor > 0:
+            saldo += valor
+            print(f"Você depositou: R${depositar}\nSaldo atual: R${salario_cliente + depositar:.2f}")
+
+        else:
+            print("Operação falhou!")
 
     # Opção de Sacar
     elif opcao == "3":
         sacar = float(input("Qual valor do saque? "))
-        print(f"Você sacou: R${sacar}\nSaldo atual: R${salario_cliente - sacar}")
+        print(f"Você sacou: R${sacar}\nSaldo atual: R${salario_cliente - sacar:.2f}")
 
     # Opção de sair
     else:
-        sair_ou_nao = str(input('Você deseja sair?\n Digite [S]sim ou [N]ão: ')).lower()
+        sair_ou_nao = str(input('Você deseja sair?\nDigite [S]sim ou [N]ão: ')).lower()
         if sair_ou_nao == "s":
             print('Você saiu')
             break
